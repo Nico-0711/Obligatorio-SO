fc101aa95c73680ccaa16f6e285c5bf980b3a817
VictoriaRodriguezDenis <victoriarodriguezdenis@gmail.com>
2025-11-16 14:27:02 -0300

    ult ver ej2

--- ejercicio2copy.c
+++ ejercicio2copy.c
@@ -1,4 +1,4 @@
-#define _XOPEN_SOURCE 500
+#define _XOPEN_SOURCE 700
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -6,182 +6,134 @@
 #include <unistd.h>
 #include <time.h>
 
-#define totalPasajeros 100            
-#define totalOficinistas 5            
-#define lecturasPorPasajero 3      
-#define cambiosPorOficinista 3  
+#define TOTAL_PASAJEROS 100
+#define TOTAL_OFICINISTAS 5
+#define LECTURAS_POR_PASAJERO 3
+#define CAMBIOS_POR_OFICINISTA 3
 
-// RECURSO COMPARTIDO: El cartel del aeropuerto. Se accede mediante un lock de lectura-escritura (rwlock)
-pthread_rwlock_t cartelVuelos;  
+// Demoras máximas según consigna
+#define MAX_DELAY_PASAJERO 3   
+#define MAX_DELAY_OFICINISTA 5 
+
+// Estado compartido
 int versionCartel = 0;
 
-sem_t mutex;            // Protege las variables de control (lectoresActivos, escritoresEsperando)
-sem_t escritura;        // Controla acceso exclusivo de escritores al recurso compartido
-sem_t cola;             // Previene inanicion de escritores 
+// Semáforos
+sem_t mutexLectores;     // protege cantidadLectores
+sem_t bloqueoEscritura;  // asegura exclusión mutua para escribir
+sem_t colaGeneral;       // evita inanición de escritores
 
-int lectoresActivos = 0;
-int escritoresEsperando = 0;
 
-void dormir_milisegundos(unsigned int milisegundos) {
-    usleep(milisegundos * 1000);
-}
+int cantidadLectores = 0;
 
-// HILO LECTOR (PASAJERO): el pasajero solo puede leer el cartel, no lo modifica y multiples pasajeros lo pueden leer a la vez.
-void *comportamientoPasajero(void *argumento) {
-    int idPasajero = *(int *)argumento;
-    free(argumento);  // libera memoria del ID recibido
-    
-    for (int veces = 0; veces < lecturasPorPasajero; ++veces) {
-        // Simular que el pasajero esta haciendo otras cosas (checkin, cafe, etc)
-        int tiempoAntesMirar = rand() % 1001;
-        dormir_milisegundos(tiempoAntesMirar);
+void *comportamientoPasajero(void *arg) {
+    int id = *(int *)arg;
+    free(arg);
 
-        sem_wait(&cola);
-        sem_wait(&mutex);
-        lectoresActivos++;
+    for (int i = 0; i < LECTURAS_POR_PASAJERO; i++) {
 
-        if (lectoresActivos == 1) {
-            sem_wait(&escritura);  // Bloquea escritores mientras haya lectores
-        }
+        // Espera simulada 
+        sleep(rand() % (MAX_DELAY_PASAJERO + 1));
 
-        sem_post(&mutex);
-        sem_post(&cola);
-        
-        printf("Pasajero %d esta mirando el cartel (version %d)\n", 
-               idPasajero, versionCartel);
+        // Entrada a la región de lectores
+        sem_wait(&colaGeneral);
+        sem_wait(&mutexLectores);
+
+        cantidadLectores++;
+        if (cantidadLectores == 1)
+            sem_wait(&bloqueoEscritura);  // primer lector bloquea escritores
+
+        sem_post(&mutexLectores);
+        sem_post(&colaGeneral);
+
+        // Lectura del cartel
+        printf("Pasajero %d está mirando el cartel (versión %d)\n", id, versionCartel);
         fflush(stdout);
-        
-        // Simular tiempo que tarda en leer la informacion del panel
-        int tiempoLeyendoPanel = 50 + (rand() % 151);  
-        dormir_milisegundos(tiempoLeyendoPanel);
- 
-        sem_wait(&mutex);
-        lectoresActivos--;
 
-        if (lectoresActivos == 0) {
-            sem_post(&escritura);  
-        }
+        sleep(rand() % (MAX_DELAY_PASAJERO + 1));
 
-        sem_post(&mutex);
+        // Salida de lector
+        sem_wait(&mutexLectores);
+
+        cantidadLectores--;
+        if (cantidadLectores == 0)
+            sem_post(&bloqueoEscritura);  // último lector libera escritores
+
+        sem_post(&mutexLectores);
     }
+
     return NULL;
 }
 
-// HILO ESCRITOR (OFICINISTA): los oficinistas modifican el cartel, solo uno a la vez y mientras este escribe ningun pasajero puede leer
-void *comportamientoOficinista(void *argumento) {
-    int idOficinista = *(int *)argumento;
-    free(argumento); 
+void *comportamientoOficinista(void *arg) {
+    int id = *(int *)arg;
+    free(arg);
 
-    for (int actualizacion = 0; actualizacion < cambiosPorOficinista; ++actualizacion) {
-        // Simular tiempo antes de necesitar actualizar (recibir info de torre, etc)
-        int tiempoAntesActualizar = rand() % 1001;  
-        dormir_milisegundos(tiempoAntesActualizar);
-        
-        sem_wait(&mutex);
-        escritoresEsperando++;
-        sem_post(&mutex);
-        sem_wait(&cola);
-        sem_wait(&escritura);
-        sem_wait(&mutex);
-        escritoresEsperando--;
-        sem_post(&mutex);
+    for (int i = 0; i < CAMBIOS_POR_OFICINISTA; i++) {
 
-        printf("Oficinista %d esta modificando el cartel\n", idOficinista);
-        fflush(stdout);  
-        
-        // Simular tiempo que tarda en actualizar vuelos, puertas, horarios
-        int tiempoActualizandoInfo = 200 + (rand() % 301); 
-        dormir_milisegundos(tiempoActualizandoInfo);
+        // Espera aleatoria antes de hacer una modificación
+        sleep(rand() % (MAX_DELAY_OFICINISTA + 1));
 
-        // Modificar el recurso compartido (incrementar version del cartel)
-        versionCartel++;
-        
-        sem_post(&cola);
-        sem_post(&escritura);
+        // Entrada del escritor
+        sem_wait(&colaGeneral);      // orden justo
+        sem_wait(&bloqueoEscritura); // exclusión mutua
+
+        // Escritura
+        printf("Oficinista %d está modificando el cartel\n", id);
+        fflush(stdout);
+
+        sleep(rand() % (MAX_DELAY_OFICINISTA + 1));
+
+        versionCartel++; // actualización real del recurso
+
+        // Salida del escritor
+        sem_post(&bloqueoEscritura);
+        sem_post(&colaGeneral);
     }
+
     return NULL;
 }
 
 int main(void) {
-    srand((unsigned int)time(NULL));
-    
-    if (sem_init(&mutex, 0, 1) != 0) {
-        perror("sem_init mutex");
-        return EXIT_FAILURE;
+    srand(time(NULL));
+
+    sem_init(&mutexLectores, 0, 1);
+    sem_init(&bloqueoEscritura, 0, 1);
+    sem_init(&colaGeneral, 0, 1);
+
+    pthread_t pasajeros[TOTAL_PASAJEROS];
+    pthread_t oficinistas[TOTAL_OFICINISTAS];
+
+    printf("=== Simulación del Cartel del Aeropuerto ===\n\n");
+
+    // Crear oficinistas (escritores)
+    for (int i = 0; i < TOTAL_OFICINISTAS; i++) {
+        int *id = malloc(sizeof(int));
+        *id = i + 1;
+        pthread_create(&oficinistas[i], NULL, comportamientoOficinista, id);
     }
 
-    if (sem_init(&escritura, 0, 1) != 0) {
-        perror("sem_init escritura");
-        sem_destroy(&mutex);
-        return EXIT_FAILURE;
+    // Crear pasajeros (lectores)
+    for (int i = 0; i < TOTAL_PASAJEROS; i++) {
+        int *id = malloc(sizeof(int));
+        *id = i + 1;
+        pthread_create(&pasajeros[i], NULL, comportamientoPasajero, id);
     }
-    
-    if (sem_init(&cola, 0, 1) != 0) {
-        perror("sem_init cola");
-        sem_destroy(&mutex);
-        sem_destroy(&escritura);
-        return EXIT_FAILURE;
-    }
-    
-    //Declarar arrays para almacenar los identificadores de hilos
-    pthread_t hilosPasajeros[totalPasajeros];
-    pthread_t hilosOficinistas[totalOficinistas];
-    
-    printf("=== Sistema de Cartel de Aeropuerto ===\n");
-    printf("Pasajeros: %d | Oficinistas: %d\n", totalPasajeros, totalOficinistas);
-    printf("Iniciando simulacion...\n\n");
-    
-    // Crear hilos ESCRITORES = Oficinistas
-    for (int i = 0; i < totalOficinistas; ++i) {
-        // Asignar memoria dinamica para pasar el ID al hilo, necesario porque el loop cambiaria el valor de 'i'
-        int *idOficinista = malloc(sizeof(int));
-        if (idOficinista == NULL) {
-            perror("malloc oficinista");
-            return EXIT_FAILURE;
-        }
-        *idOficinista = i + 1;
-        
-        if (pthread_create(&hilosOficinistas[i], NULL, comportamientoOficinista, idOficinista) != 0) {
-            perror("pthread_create oficinista");
-            free(idOficinista);
-            return EXIT_FAILURE;
-        }
-    }
-    
-    //Crear hilos LECTORES = Pasajeros
-    for (int i = 0; i < totalPasajeros; ++i) {
-        int *idPasajero = malloc(sizeof(int));
-        if (idPasajero == NULL) {
-            perror("malloc pasajero");
-            return EXIT_FAILURE;
-        }
-        *idPasajero = i + 1;
-   
-        if (pthread_create(&hilosPasajeros[i], NULL, comportamientoPasajero, idPasajero) != 0) {
-            perror("pthread_create pasajero");
-            free(idPasajero);
-            return EXIT_FAILURE;
-        }
-    }
-    
-    // Esperar a que TODOS los oficinistas terminen sus actualizaciones
-    // pthread_join bloquea hasta que el hilo termine
-    for (int i = 0; i < totalOficinistas; ++i) {
-        pthread_join(hilosOficinistas[i], NULL);
-    }
-    
-    //Esperar a que TODOS los pasajeros terminen de consultar
-    for (int i = 0; i < totalPasajeros; ++i) {
-        pthread_join(hilosPasajeros[i], NULL);
-    }
-    
-    sem_destroy(&mutex);
-    sem_destroy(&escritura);
-    sem_destroy(&cola);
 
-    printf("\n=== Simulacion finalizada ===\n");
-    printf("Version final del cartel: %d\n", versionCartel);
-    printf("Total de cambios esperados: %d\n", totalOficinistas * cambiosPorOficinista);
-    
+    // Esperar todos los hilos
+    for (int i = 0; i < TOTAL_OFICINISTAS; i++)
+        pthread_join(oficinistas[i], NULL);
+
+    for (int i = 0; i < TOTAL_PASAJEROS; i++)
+        pthread_join(pasajeros[i], NULL);
+
+    sem_destroy(&mutexLectores);
+    sem_destroy(&bloqueoEscritura);
+    sem_destroy(&colaGeneral);
+
+    printf("\n=== Fin de la Simulación ===\n");
+    printf("Versión final del cartel: %d\n", versionCartel);
+    printf("Cambios esperados: %d\n", TOTAL_OFICINISTAS * CAMBIOS_POR_OFICINISTA);
+
     return 0;
 }
